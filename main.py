2f7a17c7e6bfd5f22e5c598fcf73eba33b11cabf
Terry Clark <i.am.terryclark@gmail.com>
2015-02-19 13:30:42 +0000

    main inits game  via context

--- main.py
+++ main.py
@@ -3,25 +3,9 @@
 #Original Source: http://inventwithpython.com/slidepuzzle.py by Al Sweigart
 #Released under a "Simplified BSD" License
 
-import pygame, startScreen
+import pygame, startScreen, game, scoreBoard
+from context import *
 from pygame.locals import *
-
-def push(context):
-    game_stack.append(context)
-
-def pop():
-    try:
-        game_stack.pop()
-    except IndexError:
-        pass
-
-def top():
-    try:
-        return game_stack[-1]
-    except IndexError:
-        return None
-
-game_stack = []
 
 def run():
     pygame.init()
@@ -38,10 +22,9 @@ def run():
     while top():
         dt = clock.tick(max_fps) / 1000.0
 
-        context = top()
-        if context:
-            context.think(dt)
-
+        top_context = top()
+        if top_context:
+            top_context.think(dt)
 
 if __name__ == '__main__':
     run()
